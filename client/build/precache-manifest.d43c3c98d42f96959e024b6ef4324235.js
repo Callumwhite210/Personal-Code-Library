cf91c3638cb092566a172e2df5acf76e70557905
Callum White <callumwhite210@gmail.com>
2021-01-20 19:06:08 +1030

    gitignore

---
+++ client/build/precache-manifest.d43c3c98d42f96959e024b6ef4324235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c0873dc5ca9b8da10ef04b4b1a02241",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b566f47ffd8c9a2983ec",
+    "url": "/static/css/main.882fcff5.chunk.css"
+  },
+  {
+    "revision": "0da90f3fc6f57ebebde9",
+    "url": "/static/js/2.150b642e.chunk.js"
+  },
+  {
+    "revision": "edc8346d9905f0f87c1db77134389ba5",
+    "url": "/static/js/2.150b642e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b566f47ffd8c9a2983ec",
+    "url": "/static/js/main.ca0e1a60.chunk.js"
+  },
+  {
+    "revision": "2b8f580612459d80dc88",
+    "url": "/static/js/runtime-main.f65105ae.js"
+  }
+]);
