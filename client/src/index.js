dc5a42f3eb56151dc127e60d96df95e83f963f6d
Callum White <callumwhite210@gmail.com>
2021-02-04 14:00:29 +1030

    clean up

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,13 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const cbURI = process.env.REACT_APP_AUTH0_CB_URI
 
 ReactDOM.render(
 <Auth0Provider
 domain={domain}
 clientId={clientId}
-redirectUri={window.location.origin}>
+redirectUri={cbURI}>
     <App />
 </Auth0Provider>, 
 document.getElementById("root")
